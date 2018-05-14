c6e936a4046dfc2232e38b045f137827a13b2778
Wei Dai <wdai@wpi.edu>
2018-05-14 01:00:19 -0400

    revert

--- cufhe/test/test_api_gpu.cu
+++ cufhe/test/test_api_gpu.cu
@@ -44,10 +44,9 @@ void XorCheck(Ptxt& out, const Ptxt& in0, const Ptxt& in1) {
 }
 
 int main() {
-  cudaSetDevice(1);
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, 0);
-  uint32_t kNumSMs = 1;//prop.multiProcessorCount;
+  uint32_t kNumSMs = prop.multiProcessorCount;
   uint32_t kNumTests = kNumSMs * 32;
 
   SetSeed(); // set random seed
